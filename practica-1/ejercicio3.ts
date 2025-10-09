1cc61a5e1c1a5401757d27bc7f9bea1f4bd3ff23
Juan Carlos Conde <jconde001@gmail.com>
2025-10-09 16:11:17 +0200

    Ejercicios Practica 1

---
+++ practica-1/ejercicio3.ts
@@ -0,0 +1,25 @@
+async function obtenerTitulosDePosts(): Promise<string[]> {
+    try{
+    const respuesta = await fetch("https://jsonplaceholder.typicode.com/posts")
+    if(!respuesta){
+        throw new Error ('Error en la llamada fetch')
+    }
+    const posts: {title:string}[]= await respuesta.json()
+    const titulos = posts.map(post=>post.title)
+    }catch(error){
+        console.error("error")
+        throw error;
+    }
+}
+
+async function ejecutarObtenerTitulos() {
+  try {
+    const titulos = await obtenerTitulosDePosts();
+    console.log(`Títulos de los posts (con async/await): ${titulos}`);
+  } catch (error) {
+    console.error(`Error al obtener los títulos (con async/await): ${error}`);
+  }
+}
+
+ejecutarObtenerTitulos();
+
